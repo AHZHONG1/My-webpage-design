d0b0f59ffccb592d7a475dd007b7b6a4e39f8e20
Leo <leolai0224@gmail.com>
2022-06-08 14:06:48 +0800

    Update JavaScript

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,4 +1,25 @@
 // JavaScript source code
+
+var isMenuOpened = false;
+
 function openwindow(link) {
     window.open(link, "_self");
 }
+
+function openMenu() {
+    var menu = document.getElementsByClassName('menu');
+    if (isMenuOpened == false) {
+        isMenuOpened = true;
+        //Open menu
+        menu[0].style.visibility = "initial";
+
+    } else {
+        isMenuOpened = false;
+        //Close menu
+        menu[0].style.visibility = "hidden";
+    }
+}
+
+window.addEventListener("scroll", function () {
+    document.getElementsByClassName('menu')[0].style.visibility = "hidden";
+});
